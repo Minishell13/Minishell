853c145235e24e4cf2b5bdd84170e255f60e9270
Abdellah-Nsila <dawndawn074@gmail.com>
2025-06-03 21:37:12 +0100

    Test some asts

--- old/exec_utils.c
+++ old/exec_utils.c
@@ -6,11 +6,11 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 07:01:43 by abnsila           #+#    #+#             */
-/*   Updated: 2025/06/03 07:02:00 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/06/03 20:39:44 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include <minishell.h>
 
 t_bool	check_pwd(char *arg)
 {
